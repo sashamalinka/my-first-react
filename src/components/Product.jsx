aaa8251caa7bda083372030f8a550933fc55c550
Sasha <malinka15117kk@gmail.com>
2025-04-15 22:36:58 +0300

    asa

---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+export default function Product(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+	  <img src={props.imgUrl} alt={props.name} width="480" />
+	  <p>Price: {props.price} credits</p>
+    </div>
+  );
+};
+
