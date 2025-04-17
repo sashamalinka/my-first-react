df84a19f0a5bca8377b43bfdc28725eb80665d94
Sasha <malinka15117kk@gmail.com>
2025-04-17 22:15:12 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
-// export default function App() {
-//   return (
-//     <div>
-//       <h1>Best selling</h1>
+import Product from './Product';
+import { Alert } from './Alert';
+import { BiBody } from 'react-icons/bi';
 
-//       <Product
-//         name="Tacos With Lime"
-//         price={10.99}
-//       />
-//       <Product
-//         name="Fries and Burger"
-//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-//         price={14.29}
-//       />
-//     </div>
-//   );
-// }
+const App = () => {
+  return (
+    <>
+      <BiBody />
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <BiBody />
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <BiBody />
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <BiBody />
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Імпорт стилів нормалізації
+import 'modern-normalize';
+import App from './components/App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
